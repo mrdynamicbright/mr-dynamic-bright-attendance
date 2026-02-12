1b0af3cdd405db52843a595821e9b3c73e8569c4
Mr.Dynamic Bright <infodynamicbright@gmail.com>
2026-02-13 03:22:26 +0530

    Fixed table names properly

--- models.py
+++ models.py
@@ -3,18 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Location(db.Model):
+    __tablename__ = "locations"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     radius = db.Column(db.Integer, default=200)
 
+
 class Employee(db.Model):
+    __tablename__ = "employees"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+
 
 class Attendance(db.Model):
+    __tablename__ = "attendance"
+
     id = db.Column(db.Integer, primary_key=True)
     employee_id = db.Column(db.Integer)
     location_name = db.Column(db.String(100))
